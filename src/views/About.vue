eb9651c20f6a3d6eb5cb4af8738b145ba75aad36
Tonny <musembitonnyj@gmail.com>
2022-02-14 21:19:30 +0300

    add

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,45 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button v-on:click="openModal()">Open</button>
+  <div class="modal" :class="{ 'is-active': open }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Modal title</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <!-- Content ... -->
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" v-on:click="inGreet()">Save</button>
+        <button class="button" v-on:click="onGreet()">Cancel</button>
+      </footer>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.open = true;
+    },
+    onGreet() {
+      this.open = false;
+    },
+    inGreet() {
+      fetch().then((response) => {
+        console.log(response).catch((error) => {
+          console.log(error);
+        });
+      });
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+  <div class="primary button">Home</div>
+  <button class="button primary" v-on:click="Greet()">Add New Vehicle</button>
+  <button class="button is-dark">Load</button>
+  <div class="col-4">
+    <div class="container">
+      <div class="notification is-primary">
+        This container is <strong>centered</strong> on desktop and larger
+        viewports.
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+//import HelloWorld from "@/components/HelloWorld.vue";
+//import Footer from '@/views/Footer'
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    //Footer,
   },
 };
 </script>
